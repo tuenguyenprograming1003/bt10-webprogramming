b2182a0d357dafb4ef99062e2c5f2cce17fcf20b
tuenguyen <tueteo741@gmail.com>
2025-12-19 09:25:25 +0700

    Add demo3-thymeleaf-security: custom login, thymeleaf templates, SecurityConfig

---
+++ demo3-thymeleaf-security/src/main/java/com/example/demo3/controller/ViewController.java
@@ -0,0 +1,20 @@
+package com.example.demo3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
+
